71131f4b3f0de7812f43b634f6ee93de9d0cd62e
Saiteja4741 <mrteja2809@gmail.com>
2026-01-16 21:40:09 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/Scholarship-Hub/" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/scholarship/:id" element={<ScholarshipDetail />} />
@@ -55,7 +55,7 @@ const App = () => (
             </Route>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            {}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
